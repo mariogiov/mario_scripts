38e5fe5bd9360bc06a694b787fdf25d872426871
Mario Giovacchini <mario.giovacchini@scilifelab.se>
2014-04-11 13:07:51 +0200

    Add optional length parameter

--- count_seqs.py
+++ count_seqs.py
@@ -4,7 +4,7 @@ from itertools import islice
 import re
 import sys
 
-def count_seqs(input_file, read_max_lines=None, top_hits=25):
+def count_seqs(input_file, read_max_lines=None, top_hits=25, length=None):
 
     with open(input_file, 'r') as f:
         for num, line in enumerate(f):
@@ -15,7 +15,7 @@ def count_seqs(input_file, read_max_lines=None, top_hits=25):
     total_counts = 0
     with open(input_file, 'r') as f:
         sys.stderr.write("Reading input file \"{}\"...\n\n".format(input_file))
-        seq_counts = collections.Counter( islice(f, first_headerline+1, read_max_lines, 4) )
+        seq_counts = collections.Counter( islice(f, first_headerline+1, read_max_lines, 4)[:length] )
         total_counts = reduce( lambda x,y: x+y, seq_counts.values() )
         #for seq in islice(f, first_headerline, max_lines, 4):
         #    lengths_dict[ seq ] += 1
@@ -32,8 +32,9 @@ def count_seqs(input_file, read_max_lines=None, top_hits=25):
 if __name__=="__main__":
     parser = argparse.ArgumentParser("Get the frequencies of the most common sequences appearing in a fastq file.")
     parser.add_argument("-i", "--input", dest="input_file", required=True, help="The fastq file from which to read.")
-    parser.add_argument("-m", "--read-max-lines", dest="read_max_lines", type=int, help="The maximum number of lines to read from the file. Defaults to entire file.")
-    parser.add_argument("-t", "--top-hits", dest="top_hits", type=int, help="The number of hits to return. Defaults to 25.")
+    parser.add_argument("-m", "--read-max-lines", type=int, help="The maximum number of lines to read from the file. Defaults to entire file.")
+    parser.add_argument("-t", "--top-hits", type=int, help="The number of hits to return. Defaults to 25.")
+    parser.add_argument("-l", "--length", type=int, help="How many nts to count. Default entire read.")
 
     args = vars(parser.parse_args())
     count_seqs(**args)
